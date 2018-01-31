33b95f0d007e43c334feb3e410d584c9ca156928
erikuchiha15 <31937650+erikuchiha15@users.noreply.github.com>
2018-01-31 12:51:09 -0700

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,10 +1,27 @@
+#include<allegro5/allegro.h>
+#include<allegro5\allegro_primitives.h>
 
+int main() {
+	al_init();
+	al_init_primitives_addon();
 
-//instructions for 1-19
-//
-//intro: Write a program that asks a user for how old they are, and then repeats that number back to them in an insult.
-//
-//intermediate: Write a program that prints the numbers 256 down to 32, counting by fours
-//
-//advanced: make an account at HackerRank.com
-//do this problem: https://www.hackerrank.com/challenges/c-tutorial-struct/problem
+	ALLEGRO_DISPLAY *display = al_create_display(600, 600);
+
+	al_clear_to_color(al_map_rgb(255, 255, 255));
+
+	al_draw_rectangle(300, 209, 50, 50, al_map_rgb(50, 50, 50), 5);
+
+	al_draw_filled_circle(300, 300, 50, al_map_rgb(80, 200, 20));
+
+	al_draw_rectangle(300, 150, 500, 250, al_map_rgb(40, 20, 29),5);
+
+	al_draw_filled_circle(500, 300, 50, al_map_rgb(20, 20, 20));
+
+	al_draw_filled_triangle(359, 200, 50, 10, 60, 70,al_map_rgb(200, 50, 60));
+
+	al_flip_display();
+
+	al_rest(10);
+
+	al_destroy_display(display);
+}
